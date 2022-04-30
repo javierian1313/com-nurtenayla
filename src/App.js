db6ace9c4899f7a9bd179738f4a309e37d083712
Javier Hardman <javierian1313@Javiers-MacBook-Pro.local>
2022-04-30 01:48:09 -0600

    added bday message

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
     return (
         <div className='App'>
             <header className='App-header'>
-                <div>
-                    <img src={catBirthday} alt='cat birthday' style={{ maxWidth: '95%' }} />
-                </div>
-                <p>{`<3 From BBB, i luvv uuuuuuu BBP <3`}</p>
+                <p style={{ marginBottom: 5, marginTop: 5 }}>{`<3 Happy Birthday BBP!!! <3`}</p>
+                <img src={catBirthday} alt='cat birthday' style={{ maxWidth: '95%' }} />
+                <p style={{ marginBottom: 5, marginTop: 5 }}>{`<3 Have an absolutely fabulous day!!! <3`}</p>
+                <p style={{ marginBottom: 5, marginTop: 5 }}>{`<3 i luvv uuuuuuu <3`}</p>
             </header>
         </div>
     );
